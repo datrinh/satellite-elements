3de9555855520f7804185bab78f9f01cdaa4a936
Duc <duc@hello-charles.com>
2021-10-28 15:37:54 +0200

    working example with custom element

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import "document-register-element/build/document-register-element";
+import vueCustomElement from "vue-custom-element";
 import Vue from "vue";
 import App from "./App.vue";
+import NewsletterOptIn from "./components/NewsletterOptIn.vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(vueCustomElement);
+
+Vue.customElement("charles-newsletter", NewsletterOptIn);
 
 new Vue({
   render: (h) => h(App),

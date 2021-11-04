c405270f931ffe8976d26de44ce434bb0dfbb76a
Duc <duc@hello-charles.com>
2021-11-04 09:36:56 +0100

    b

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // import "document-register-element/build/document-register-element";
-import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter";
-import "@webcomponents/webcomponentsjs/webcomponents-loader";
+// import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter";
+// import "@webcomponents/webcomponentsjs/webcomponents-loader";
 import vueCustomElement from "vue-custom-element";
 import VueCompositionAPI from "@vue/composition-api";
 import Vue from "vue";
